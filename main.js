2a1dc0c3e6cc6d2fc609c7b56045052821490c2b
Ghulam Muneer uddin <gmugsk@gmail.com>
2024-03-21 13:23:52 +0500

    assignment 06

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+/*Stripping Names: Store a person’s name, and include some whitespace
+ characters at the beginning and end of the name. Make sure you
+   use each character combination, "\t" and "\n", at least once. Print the
+   name once, so the whitespace around the name is displayed.
+    Then print the name after striping the white spaces*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const myName = "   Ghulam Muneer Uddin  ";
+// \t used for tab/space
+// const myName: string=" \t Ghulam Muneer Uddin";
+// \n used for new line
+// const myName: string=" \n Ghulam Muneer Uddin";
+console.log(myName);
+//console.log(myName.trim());
---
+++ main.ts
@@ -0,0 +1,17 @@
+/*Stripping Names: Store a person’s name, and include some whitespace 
+ characters at the beginning and end of the name. Make sure you
+   use each character combination, "\t" and "\n", at least once. Print the
+   name once, so the whitespace around the name is displayed. 
+    Then print the name after striping the white spaces*/
+
+    const myName: string =    "   Ghulam Muneer Uddin  ";
+
+    // \t used for tab/space
+   // const myName: string=" \t Ghulam Muneer Uddin";
+
+    // \n used for new line
+   // const myName: string=" \n Ghulam Muneer Uddin";
+
+    console.log(myName);
+
+ //console.log(myName.trim());
